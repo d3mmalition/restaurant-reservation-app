263802baef83eda94fc15c1b95bf81a262dbf725
d3mmalition <ecarr.gardner@gmail.com>
2023-02-27 16:55:49 -0500

    updated createTablesTable

--- starter-restaurant-reservation-main/back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ starter-restaurant-reservation-main/back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -1,13 +1,17 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("reservations", (table) => {
-    table.increments("reservation_id").primary();
-    table.string("first_name").notNullable();
-    table.string("last_name").notNullable();
-    table.string("mobile_number").notNullable();
-    table.date("reservation_date").notNullable();
-    table.time("reservation_time").notNullable();
-    table.integer("people").notNullable();
-    table.timestamps(true, true);
+  return knex.schema.hasTable("reservations").then(function (exists) {
+    if (!exists) {
+      return knex.schema.createTable("reservations", (table) => {
+        table.increments("reservation_id").primary();
+        table.string("first_name").notNullable();
+        table.string("last_name").notNullable();
+        table.string("mobile_number").notNullable();
+        table.date("reservation_date").notNullable();
+        table.time("reservation_time").notNullable();
+        table.integer("people").notNullable();
+        table.timestamps(true, true);
+      });
+    }
   });
 };
 
