6d167ae21c7b22e1b6fa598b856add58eb8e5aa0
d3mmalition <ecarr.gardner@gmail.com>
2023-02-27 10:05:49 -0500

    added frontend erroralerts

--- starter-restaurant-reservation-main/front-end/src/dashboard/Dashboard.js
+++ starter-restaurant-reservation-main/front-end/src/dashboard/Dashboard.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { useHistory,  } from "react-router";
+import useQuery from "../utils/useQuery";
 import { listReservations } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
+import { next, today, previous } from "../utils/date-time";
+import ReservationsList from "../reservations/ReservationsList";
+import TablesList from "../tables/TablesList";
 
 /**
  * Defines the dashboard page.
@@ -9,9 +14,19 @@ import ErrorAlert from "../layout/ErrorAlert";
  * @returns {JSX.Element}
  */
 function Dashboard({ date }) {
-  const [reservations, setReservations] = useState([]);
-  const [reservationsError, setReservationsError] = useState(null);
 
+  const history = useHistory();
+  const query = useQuery();
+  const [reservations, setReservations] = useState([]);
+  const [reservationsError, setReservationsError] = useState(null); 
+  //sets the date to the date defined in the url
+  date = query.get("date") || date;
+
+  //converts the date from the URL to [day] [month] [date] [year] format
+  let dateConvert = new Date(`${date}T00:00:00`);
+  let displayDate = dateConvert.toDateString();
+
+  //loads the dashboard with the reservation info.
   useEffect(loadDashboard, [date]);
 
   function loadDashboard() {
@@ -22,15 +37,46 @@ function Dashboard({ date }) {
       .catch(setReservationsError);
     return () => abortController.abort();
   }
-
+  
   return (
     <main>
-      <h1>Dashboard</h1>
-      <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for date</h4>
+      <div>
+        <ErrorAlert error={reservationsError} />
       </div>
-      <ErrorAlert error={reservationsError} />
-      {JSON.stringify(reservations)}
+      <div className="container-fluid col-12 text-center">
+        <h1 className="center-align">Dashboard</h1>
+          <h4 className="bg-white mb-2">Reservations for {displayDate}</h4>
+        <div>
+          <button className="btn btn-outline-info" onClick={() => history.push(`/dashboard?date=${previous(date)}`)}>
+            Previous Day
+          </button>
+          <button className="btn btn-outline-info mx-2" onClick={() => history.push(`/dashboard/?date=${today()}`)}>
+            Today
+          </button>
+          <button className="btn btn-outline-info" onClick={() => history.push(`/dashboard/?date=${next(date)}`)}>
+            Next Day
+          </button>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+        <div className= "col">
+            <h4>Reservations</h4>
+            <div>
+              <ReservationsList reservations={reservations}/>
+            </div>
+        </div>
+        <div className="col">
+          <h4>
+            Tables
+          </h4>
+          <div>
+            <TablesList />
+          </div>
+        </div>
+        </div>
+      </div>
+      
     </main>
   );
 }
--- starter-restaurant-reservation-main/front-end/src/layout/Routes.js
+++ starter-restaurant-reservation-main/front-end/src/layout/Routes.js
@@ -1,9 +1,13 @@
 import React from "react";
-
 import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
+import NewTable from "../tables/NewTable";
+import SeatReservation from "../reservations/SeatReservation";
+import SearchByNumber from "../reservations/SearchByNumber";
+import EditReservation from "../reservations/EditReservation";
+import CreateReservation from "../reservations/NewReservation";
 
 /**
  * Defines all the routes for the application.
@@ -13,17 +17,44 @@ import { today } from "../utils/date-time";
  * @returns {JSX.Element}
  */
 function Routes() {
+
+  
+
   return (
     <Switch>
+
       <Route exact={true} path="/">
         <Redirect to={"/dashboard"} />
       </Route>
+
       <Route exact={true} path="/reservations">
         <Redirect to={"/dashboard"} />
       </Route>
-      <Route path="/dashboard">
-        <Dashboard date={today()} />
+
+      <Route exact path="/reservations/new">
+        <CreateReservation />
       </Route>
+
+      <Route path="/tables/new">
+        <NewTable />
+      </Route>
+
+      <Route path="/dashboard">
+        <Dashboard date={today()}/>
+      </Route>
+
+      <Route path="/reservations/:reservation_id/seat">
+        <SeatReservation />
+      </Route>
+
+      <Route path="/search">
+        <SearchByNumber />
+      </Route>
+      
+      <Route path="/reservations/:reservation_id/edit">
+        <EditReservation />
+      </Route>
+
       <Route>
         <NotFound />
       </Route>
