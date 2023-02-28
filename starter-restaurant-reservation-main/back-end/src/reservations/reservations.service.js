b02c6889fa7e6ab14fbf63571586597c927fcb32
d3mmalition <ecarr.gardner@gmail.com>
2023-02-27 20:38:56 -0500

    replaced reservations

--- starter-restaurant-reservation-main/back-end/src/reservations/reservations.service.js
+++ starter-restaurant-reservation-main/back-end/src/reservations/reservations.service.js
@@ -1,28 +1,64 @@
-/**
- * Defines the router for reservation resources.
- *
- * @type {Router}
- */
+const knex = require("../db/connection");
 
-const router = require("express").Router();
-const methodNotAllowed = require("../errors/methodNotAllowed");
-const controller = require("./reservations.controller");
+function createReservation(newReservation) {
+    return knex("reservations")
+      .insert(newReservation)
+      .returning("*")
+      .then((createdRecord) => createdRecord[0]);
+  }
 
-router
-  .route("/:reservation_Id/status")
-  .put(controller.update)
-  .all(methodNotAllowed);
+function listReservations(date) {
+    if (date) {
+    return knex("reservations")
+        .select("*")
+        .whereNot({ status : "finished"})
+        .andWhere({ reservation_date : date })
+        .orderBy("reservation_time");
+    }
+    else {
+        return knex("reservations")
+        .select("*")
+    }
+}
 
-router
-  .route("/:reservation_Id")
-  .get(controller.read)
-  .put(controller.modify)
-  .all(methodNotAllowed);
+function listResById(id) {
+    return knex("reservations")
+        .select("*")
+        .where({ reservation_id : id})
+        .first();
+}
 
-router
-  .route("/")
-  .get(controller.list)
-  .post(controller.create)
-  .all(methodNotAllowed);
+function updateStatus(id, newStatus) {
+    return knex("reservations")
+        .select("*")
+        .where({ reservation_id: id})
+        .update({ status : newStatus })
+        .returning("*")
+        .then((createdRecord) => createdRecord[0]);
+}
 
-module.exports = router;
+function search(mobile_number) {
+    return knex("reservations")
+      .whereRaw(
+        "translate(mobile_number, '() -', '') like ?",
+        `%${mobile_number.replace(/\D/g, "")}%`
+      )
+      .orderBy("reservation_date");
+  }
+
+  function updateReservation(updatedReservation, reservation_id) {
+    return knex("reservations")
+        .select("*")
+        .where({ reservation_id : reservation_id })
+        .update(updatedReservation, "*")
+        .then((createdRecord) => createdRecord[0]);
+  }
+
+module.exports = {
+    createReservation,
+    listReservations,
+    listResById,
+    updateStatus,
+    search,
+    updateReservation,
+}
